7cd5cbc23a5f9a280cefb9d15b20c7d10a7561b6
thebobak <bshafiei@gmail.com>
2017-12-08 16:49:33 -0500

    Added FAQ template

---
+++ template-faq.php
@@ -0,0 +1,64 @@
+<?php
+/* Template Name: FAQ Page */
+?>
+
+
+<?php // Query for Child Pages //
+
+  $query = new WP_Query( array(
+    'post_type'      => 'faq',
+    'posts_per_page' => -1,
+    'order'          => 'ASC',
+    'orderby'        => 'menu_order'
+    ));
+?>
+
+<?php get_header(); ?>
+
+<div class="entry-content-page">
+  <section>
+    <h1 class="entry-title"><?php the_title(); ?></h1> <!-- Page Title -->
+    
+    <?php //////////////////////////////
+    // DISPLAY THE PAGE CONTENT //
+    ////////////////////////////// ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        
+          <?php the_content(); ?> <!-- Page Content -->
+          <?php endwhile; // END of Page Content ?>
+          <?php wp_reset_query(); //resetting the page query ?>
+  </section>
+<section>
+
+  <button type="button" onclick="expand()">Expand All</button>
+<button type="button" onclick="collapse()">Collapse All</button>
+  <!-- /////////////////////////// -->
+<?php // THE CONTENT LOOP // ?>
+<?php $loopcounter = 1; ?>
+<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+
+  <h4><a data-toggle="collapse" href="#collapse-<?php echo "$loopcounter"; ?>" aria-expanded="false" aria-controls="collapse-<?php echo "$loopcounter"; ?>"><?php the_title(); ?></a></h4>
+
+  <div id="collapse-<?php echo "$loopcounter"; ?>" class="collapse" role="tabpanel" aria-labelledby="heading-<?php echo "$loopcounter"; ?>" data-parent="#accordion">
+    <p> <?php the_content(); ?> </p>
+  </div>
+
+<?php $loopcounter += 1; ?>
+<?php endwhile; wp_reset_postdata();?>
+</section>
+        </div><!-- .entry-content-page -->
+
+
+
+<?php get_footer(); ?>
+  <script>
+    function expand() {
+            $('.collapse').collapse('show');
+        }
+    function collapse() {
+            $('.collapse').collapse('hide');
+        }
+    </script>
+    
+    
